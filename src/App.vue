c66ca0219b59ace33fb61e63c1ab73af3d64829d
chenql <394869042@qq.com>
2020-05-24 23:26:31 +0800

    4-5目录结构设置

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-// import axios from 'axios'
 import jsonp from 'jsonp'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
 
   data(){
     return {
-      age:30,
-      data:''
     }
   },
 
   mounted(){
-    //CROS跨域解决(后端改)
-    // let url = 'https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list'
-    // axios.get(url).then(()=>{
-      
-    // })
 
-    //JSONP跨域解决(前端加插件、后端小改)
-    // let url2 = '/search/hotwords'
-    // jsonp(url2,(err,res)=>{
-    //   let result = res;
-    //   this.data = result;
-    // })
-
-    //接口代理
-    let url2 = '/api/search/hotwords'
-    jsonp(url2,(err,res)=>{
-      let result = res;
-      this.data = result;
-    })
   }
 
 }
