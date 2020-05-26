46559deccb5dfa1804ed632413f41271c89e6caa
chenql <394869042@qq.com>
2020-05-26 23:41:08 +0800

    4-10 接口环境设置

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from'vue-axios'
-
 import App from './App.vue'
+import env from './env'
 
 // 根据前端的跨域方式做调整
 axios.defaults.baseURL = '/api';
 axios.defaults.timeout = 8000
+//根据环境变量获取不同的请求地址
+axios.defaults.baseURL = env.baseURL;
 
 // 接口错误拦截
 axios.interceptors.response.use(function(response){
@@ -15,7 +17,7 @@ axios.interceptors.response.use(function(response){
   if(res.status == 0){
     return res.data;
   }else if(res.status == 10){
-    windows.location.href = '/#/login';
+    window.location.href = '/#/login';
   }else {
     alert(res.msg);
   }
