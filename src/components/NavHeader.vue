acf80fad99e0884b0b489318a9c6043ec94cdd59
chenql <394869042@qq.com>
2020-05-31 01:18:14 +0800

    5-4

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -11,7 +11,9 @@
         <div class="topbar-user">
           <a href="javascript:;">登录</a>
           <a href="javascript:;">注册</a>
-          <a href="javascript:;" class="my-cart"><span class="icon-cart"></span>购物车</a>
+          <a href="javascript:;" class="my-cart">
+            <span class="icon-cart"></span>购物车
+          </a>
         </div>
       </div>
     </div>
@@ -23,7 +25,76 @@
         <div class="header-menu">
           <div class="item-menu">
             <span>小米手机</span>
-            <div class="children"></div>
+            <div class="children">
+              <ul>
+                <li class="product">
+                  <a href target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/f515ab05232ed14ccd78ec67e024495a.png"
+                      />
+                    </div>
+                    <div class="pro-name">小米CC9</div>
+                    <div class="pro-price">1799</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/f515ab05232ed14ccd78ec67e024495a.png"
+                      />
+                    </div>
+                    <div class="pro-name">小米CC9</div>
+                    <div class="pro-price">1799</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/f515ab05232ed14ccd78ec67e024495a.png"
+                      />
+                    </div>
+                    <div class="pro-name">小米CC9</div>
+                    <div class="pro-price">1799</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/f515ab05232ed14ccd78ec67e024495a.png"
+                      />
+                    </div>
+                    <div class="pro-name">小米CC9</div>
+                    <div class="pro-price">1799</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/f515ab05232ed14ccd78ec67e024495a.png"
+                      />
+                    </div>
+                    <div class="pro-name">小米CC9</div>
+                    <div class="pro-price">1799</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <a href target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/f515ab05232ed14ccd78ec67e024495a.png"
+                      />
+                    </div>
+                    <div class="pro-name">小米CC9</div>
+                    <div class="pro-price">1799</div>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div class="item-menu">
             <span>RedMi红米</span>
@@ -36,7 +107,7 @@
         </div>
         <div class="header-search">
           <div class="wrapper">
-            <input type="text" name="keyname">
+            <input type="text" name="keyname" />
             <a href="javascript:;"></a>
           </div>
         </div>
@@ -45,121 +116,166 @@
   </div>
 </template>
 <script>
-  export default {
-    name: 'nav-header',
-  }
+export default {
+  name: "nav-header"
+};
 </script>
 <style lang="scss">
-  @import './../assets/scss/base.scss';
-  @import './../assets/scss/mixin.scss';
+@import "./../assets/scss/base.scss";
+@import "./../assets/scss/mixin.scss";
+@import "./../assets/scss/config.scss";
 
-  .header {
-    .nav-topbar {
-      height: 39px;
-      line-height: 39px;
-      background-color: #333333;
-      color: #B0B0B0;
-
-      .container {
-        @include flex();
-
-        a {
-          display: inline-block;
-          color: #B0B0B0;
-          margin-right: 17px;
-        }
-
-        .my-cart {
-          width: 110px;
-          background-color: #FF6600;
-          text-align: center;
-          color: #ffffff;
-
-          .icon-cart {
-            @include bgImg(16px, 12px, '/imgs/icon-cart-checked.png');
-            margin-right: 4px;
-          }
+.header {
+  .nav-topbar {
+    height: 39px;
+    line-height: 39px;
+    background-color: #333333;
+    color: #b0b0b0;
+    .container {
+      @include flex();
+      a {
+        display: inline-block;
+        color: #b0b0b0;
+        margin-right: 17px;
+      }
+      .my-cart {
+        width: 110px;
+        background-color: #ff6600;
+        text-align: center;
+        color: #ffffff;
+        .icon-cart {
+          @include bgImg(16px, 12px, "/imgs/icon-cart-checked.png");
+          margin-right: 4px;
         }
       }
     }
+  }
 
-    .nav-header {
-      .container {
-        height: 112px;
-        @include flex(); 
-        .header-logo {
+  .nav-header {
+    .container {
+      position: relative;
+      height: 112px;
+      @include flex();
+      .header-logo {
+        display: inline-block;
+        width: 55px;
+        height: 55px;
+        background-color: #ff6600;
+        a {
           display: inline-block;
-          width: 55px;
+          width: 110px;
           height: 55px;
-          background-color: #FF6600;
-
-          a {
-            display: inline-block;
-            width: 110px;
-            height: 55px;
-
-            &:before {
-              content: ' ';
-              @include bgImg(55px, 55px, '/imgs/mi-logo.png', 55px);
-              transition: all 0.3s;
-            }
-
-            &:after {
-              content: ' ';
-              @include bgImg(55px, 55px, '/imgs/mi-home.png', 55px);
-            }
-
-            &:hover:before {
-              margin-left: -55px;
-              transition: all 0.3s;
-            }
+          &:before {
+            content: " ";
+            @include bgImg(55px, 55px, "/imgs/mi-logo.png", 55px);
+            transition: all 0.3s;
+          }
+          &:after {
+            content: " ";
+            @include bgImg(55px, 55px, "/imgs/mi-home.png", 55px);
+          }
+          &:hover:before {
+            margin-left: -55px;
+            transition: all 0.3s;
           }
         }
+      }
 
-        .header-menu {
+      .header-menu {
+        display: inline-block;
+        width: 643px;
+        padding-left: 209px;
+        .item-menu {
           display: inline-block;
-          width: 643px;
-          padding-left: 209px;
-
-          .item-menu {
-            display: inline-block;
-            color: #333333;
-            font-weight: bold;
-            font-size: 16px;
-            line-height: 112px;
-            margin-right: 20px;
-
-            span {
-              cursor: pointer;
-
+          color: #333333;
+          font-weight: bold;
+          font-size: 16px;
+          line-height: 112px;
+          margin-right: 20px;
+          span {
+            cursor: pointer;
+          }
+          &:hover {
+            color: $colorA;
+            .children {
+              height: 220px;
             }
-
-            &:hover {}
+          }
+          .children {
+            position: absolute;
+            top: 112px;
+            left: 0;
+            width: 1226px;
+            height:220px;
+            border-top: 1px solid #e5e5e5;
+            box-shadow: 0px 7px 6px 0px rgba(0, 0, 0, 0.11);
+            .product {
+              position: relative;
+              float: left;
+              width: 16.6%;
+              height: 220px;
+              font-size: 12px;
+              line-height: 12px;
+              text-align: center;
+              a {
+                display: inline-block;
+              }
+              img {
+                width: auto;
+                height: 111px;
+                margin-top: 26px;
+              }
+              .pro-img {
+                height: 137px;
+              }
+              .pro-name {
+                font-weight: bold;
+                margin-top: 19px;
+                margin-bottom: 8px;
+                color: $colorB;
+              }
+              .pro-price {
+                color: $colorA;
+              }
+              &:before {
+                content: " ";
+                position: absolute;
+                top: 28px;
+                right: 0;
+                border-left: 1px solid $colorF;
+                height: 100px;
+                width: 1px;
+              }
+              &:last-child:before{
+                display:none;
+              }
+            }
           }
         }
+      }
 
-        .header-search {
-          width: 319px;
-          .wrapper {
+      .header-search {
+        width: 319px;
+        .wrapper {
+          height: 50px;
+          border: 1px solid #e0e0e0;
+          display: flex;
+          align-items: center;
+          input {
+            border: none;
+            border-right: 1px solid #e0e0e0;
+            width: 264px;
             height: 50px;
-            border: 1px solid #E0E0E0;
-            display: flex;
-            align-items: center;
-            input {
-              border: none;
-              border-right: 1px solid #E0E0E0;
-              width: 264px;
-              height: 50px;
-              padding-left: 14px;
-              box-sizing: border-box;
-            }
-            a {
-              @include bgImg(18px, 18px, '/imgs/icon-search.png');
-              margin-left: 17px;
-            }
+            padding-left: 14px;
+            box-sizing: border-box;
+          }
+          a {
+            @include bgImg(18px, 18px, "/imgs/icon-search.png");
+            margin-left: 17px;
           }
         }
       }
     }
   }
+}
 </style>
