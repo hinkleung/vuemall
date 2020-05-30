063de751254e82c201be7dfbf13b09bac41e5048
chenql <394869042@qq.com>
2020-05-31 00:16:52 +0800

    5-3

--- src/App.vue
+++ src/App.vue
@@ -12,35 +12,16 @@ export default {
 
   data(){
     return {
-      res:{}
     }
   },
 
   mounted(){
-    // 第一种 本地加载请求静态json文件的方式
-    // this.axios.get('/mock/user/login.json').then((res)=>{
-    //     this.res=res;
-    // });
-    // 第二种 easy-mock 换base_url
-    // this.axios.get('/user/login').then((res)=>{
-    //     this.res=res;
-    // });
-    // 第三种 本地集成mockjs实现数据mock
-    this.axios.get('/user/login').then((res)=>{
-        this.res=res;
-    });
+
   }
 
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/scss/reset.scss'
 </style>
