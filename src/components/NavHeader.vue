c58e302219d355bebdde0d997516c5a9f9934a7c
chenql <394869042@qq.com>
2020-06-16 23:16:54 +0800

    修改clearfix的bug 10-1 购物车Order——-Header组件

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -213,30 +213,6 @@
       position: relative;
       height: 112px;
       @include flex();
-      .header-logo {
-        display: inline-block;
-        width: 55px;
-        height: 55px;
-        background-color: #ff6600;
-        a {
-          display: inline-block;
-          width: 110px;
-          height: 55px;
-          &:before {
-            content: " ";
-            @include bgImg(55px, 55px, "/imgs/mi-logo.png", 55px);
-            transition: all 0.3s;
-          }
-          &:after {
-            content: " ";
-            @include bgImg(55px, 55px, "/imgs/mi-home.png", 55px);
-          }
-          &:hover:before {
-            margin-left: -55px;
-            transition: all 0.3s;
-          }
-        }
-      }
 
       .header-menu {
         display: inline-block;
