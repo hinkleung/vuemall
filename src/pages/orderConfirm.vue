df4f26d61e4a60883b1da7affbfe0c63229352d8
chenql <394869042@qq.com>
2020-06-22 23:44:21 +0800

    10-3 收货地址删除

--- src/pages/orderConfirm.vue
+++ src/pages/orderConfirm.vue
@@ -35,14 +35,14 @@
                 <div class="phone">{{item.receiverMobile}}</div>
                 <div class="street">{{item.receiverProvince + ' ' + item.receiverCity + ' ' + item.receiverDistrict + ' ' + item.receiverAddress}}</div>
                 <div class="action">
-                  <a href="javascript:;" class="fl">
+                  <a href="javascript:;" class="fl" @click="delAddress(item)">
                     <svg class="icon icon-del">
-                      <use xlink:href="#icon-del" />
+                      <use xlink:href="#icon-del"></use>
                     </svg>
                   </a>
-                  <a href="javascript:;" class="fr">
+                  <a href="javascript:;" class="fr" >
                     <svg class="icon icon-edit">
-                      <use xlink:href="#icon-edit" />
+                      <use xlink:href="#icon-edit"></use>
                     </svg>
                   </a>
                 </div>
@@ -146,6 +146,9 @@
     <modal
       title="删除确认"
       btnType="1"
+      :showModal="showDelModal"
+      @cancel="showDelModal=false"
+      @submit="submitAddress"
     >
       <template v-slot:body>
         <p>您确认要删除此地址吗？</p>
@@ -154,6 +157,7 @@
   </div>
 </template>
 <script>
+import Modal from './../components/Modal'
 export default {
   name: "order-confirm",
   data() {
@@ -162,7 +166,13 @@ export default {
       cartList:[],//购物车中需要结算的商品列表
       cartTotalPrice:0,//商品总金额
       count:0,//商品结算数量
+      checkedItem:{},//选中的商品对象
+      userAction:'',//用户行为 0新增 1编辑 2删除
+      showDelModal:false,//是否显示删除弹框
     }
+  },
+  components:{
+    Modal
   },
   mounted(){
     this.getAddressList();
@@ -173,6 +183,33 @@ export default {
       this.axios.get('/shippings').then((res)=>{
         this.list = res.list;
       })
+    },
+    delAddress(item){
+      this.checkedItem = item;
+      this.userAction = 2;
+      this.showDelModal = true;
+    },
+    //地址删除、编辑、新增功能
+    submitAddress(){
+      let {checkedItem,userAction} = this;
+      let method,url;
+      if(userAction == 0){
+        method = 'post' , url = '/shippings';
+      }else if(userAction == 1){
+        method = 'put' , url = `/shippings/${checkedItem.id}`;
+      }else {
+        method = 'delete' , url = `/shippings/${checkedItem.id}`;
+      }
+      this.axios[method](url).then(()=>{
+        this.closeModal();
+        this.getAddressList();
+        this.$message.success('操作成功');
+      });
+    },
+    closeModal(){
+      this.checkedItem = {};
+      this.userAction = 0;
+      this.showDelModal = false;
     },
     getCartList(){
         this.axios.get('/carts').then((res)=>{
