9a5f9f5274dfb55b3132f505b6df28914e7c9add
chenql <394869042@qq.com>
2020-06-29 21:18:43 +0800

    12-6 地址编辑和订单提交

--- src/pages/orderConfirm.vue
+++ src/pages/orderConfirm.vue
@@ -30,7 +30,7 @@
           <div class="item-address">
             <h2 class="addr-title" >收货地址</h2>
             <div class="addr-list clearfix">
-              <div class="addr-info" v-for="(item,index) in list" :key="index">
+              <div class="addr-info" :class="{'checked':index == checkIndex}" @click="checkIndex=index" v-for="(item,index) in list" :key="index">
                 <h2>{{item.receiverName}}</h2>
                 <div class="phone">{{item.receiverMobile}}</div>
                 <div class="street">{{item.receiverProvince + ' ' + item.receiverCity + ' ' + item.receiverDistrict + ' ' + item.receiverAddress}}</div>
@@ -40,7 +40,7 @@
                       <use xlink:href="#icon-del"></use>
                     </svg>
                   </a>
-                  <a href="javascript:;" class="fr" >
+                  <a href="javascript:;" class="fr" @click="editAddressModal(item)">
                     <svg class="icon icon-edit">
                       <use xlink:href="#icon-edit"></use>
                     </svg>
@@ -99,7 +99,7 @@
           </div>
           <div class="btn-group">
             <a href="/#/cart" class="btn btn-default btn-large">返回购物车</a>
-            <a href="javascript:;" class="btn btn-large" >去结算</a>
+            <a href="javascript:;" class="btn btn-large" @click="orderSubmit">去结算</a>
           </div>
         </div>
       </div>
@@ -173,6 +173,7 @@ export default {
       userAction:'',//用户行为 0新增 1编辑 2删除
       showDelModal:false,//是否显示删除弹框
       showEditModal:false,//是否显示新增或编辑弹框
+      checkIndex:0,//当前收货地址选中的索引
     }
   },
   components:{
@@ -187,6 +188,11 @@ export default {
       this.axios.get('/shippings').then((res)=>{
         this.list = res.list;
       })
+    },
+    editAddressModal(item){
+      this.userAction = 1;
+      this.checkedItem = item;
+      this.showEditModal = true;
     },
     openAddressModal(){
       this.userAction = 0;
@@ -260,6 +266,25 @@ export default {
             this.count += item.quantity;
           })
       })
+    },
+    //订单提交
+    orderSubmit(){
+      let item = this.list[this.checkIndex];
+      if(!item){
+        this.$message.error('请选择一个收货地址');
+        return;
+      }
+      this.axios.post('/orders',{
+        shippingId:item.id
+      }).then((res)=>{
+        this.$router.push({
+          path:'/order/pay',
+          query:{
+            orderNo:res.orderNo
+          }
+        })
+      })
+
     }
   }
 };
