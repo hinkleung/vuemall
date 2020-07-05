3e2b91f8372ac7eb8667bbd9335c4df0b3d42c0d
chenql <394869042@qq.com>
2020-07-05 18:29:29 +0800

    NoData组件

--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -8,6 +8,7 @@
     <div class="wrapper">
       <div class="container">
         <div class="order-box">
+          <loading v-if="loading"></loading>
           <div class="order" v-for="(order,index) in list" :key="index">
             <div class="order-title">
               <div class="item-info fl">
@@ -29,7 +30,7 @@
               <div class="good-box fl">
                 <div class="good-list" v-for="(item,i) in order.orderItemVoList" :key="i">
                   <div class="good-img">
-                    <img v-lazy="item.productImage" alt="">
+                    <img v-lazy="item.productImage" alt />
                   </div>
                   <div class="good-name">
                     <div class="p-name">{{item.productName}}</div>
@@ -45,122 +46,133 @@
               </div>
             </div>
           </div>
-            <img src="/imgs/loading-svg/loading-spinning-bubbles.svg" alt="" v-show="loading">
-          </div>
+          <!-- <img src="/imgs/loading-svg/loading-spinning-bubbles.svg" alt="" v-show="loading"> -->
         </div>
+        <NoData v-if="!loading && list.length==0"></NoData>
       </div>
     </div>
+  </div>
 </template>
 <script>
-  import OrderHeader from './../components/OrderHeader'
-
-  export default{
-    name:'order-list',
-    components:{
-      OrderHeader,
-
-    },
-    data(){
-      return {
-        list:[]
-      }
-    },
-    mounted(){
-      this.getOrderList();
-    },
-    methods:{
-      getOrderList(){
-        this.axios.get('/orders').then((res)=>{
-          this.list = res.list;
+import OrderHeader from "./../components/OrderHeader";
+import Loading from "./../components/Loading";
+import NoData from "./../components/NoData";
+export default {
+  name: "order-list",
+  components: {
+    OrderHeader,
+    Loading,
+    NoData
+  },
+  data() {
+    return {
+      loading: true,
+      list: []
+    };
+  },
+  mounted() {
+    this.getOrderList();
+  },
+  methods: {
+    getOrderList() {
+      this.axios
+        .get("/orders")
+        .then(res => {
+          this.loading = false;
+          this.list = []||res.list;
         })
-      },
-      goPay(orderNo){
-        this.$router.push({
-          path:'/order/pay',
-          query:{
-            orderNo
-          }
-        })
-      }
+        .catch(() => {
+          this.loading = false;
+        });
+    },
+    goPay(orderNo) {
+      this.$router.push({
+        path: "/order/pay",
+        query: {
+          orderNo
+        }
+      });
     }
   }
+};
 </script>
 <style lang="scss">
-  @import './../assets/scss/config.scss';
-  @import './../assets/scss/mixin.scss';
-  .order-list{
-    .wrapper{
-      background-color:$colorJ;
-      padding-top:33px;
-      padding-bottom:110px;
-      .order-box{
-        .order{
-          @include border();
-          background-color:$colorG;
-          margin-bottom:31px;
-          &:last-child{
-            margin-bottom:0;
-          }
-          .order-title{
-            @include height(74px);
-            background-color:$colorK;
-            padding:0 43px;
-            font-size:16px;
-            color:$colorC;
-            .item-info{
-              span{
-                margin:0 9px;
-              }
-            }
-            .money{
-              font-size:26px;
-              color:$colorB;
+@import "./../assets/scss/config.scss";
+@import "./../assets/scss/mixin.scss";
+.order-list {
+  .wrapper {
+    background-color: $colorJ;
+    padding-top: 33px;
+    padding-bottom: 110px;
+    .order-box {
+      .order {
+        @include border();
+        background-color: $colorG;
+        margin-bottom: 31px;
+        &:last-child {
+          margin-bottom: 0;
+        }
+        .order-title {
+          @include height(74px);
+          background-color: $colorK;
+          padding: 0 43px;
+          font-size: 16px;
+          color: $colorC;
+          .item-info {
+            span {
+              margin: 0 9px;
             }
           }
-          .order-content{
-            padding:0 43px;
-            .good-box{
-              width:88%;
-              .good-list{
-                display: flex;
-                align-items: center;
-                height:145px;
-                .good-img{
-                  width:112px;
-                  img{
-                    width:100%;
-                  }
-                }
-                .good-name{
-                  font-size:20px;
-                  color:$colorB;
+          .money {
+            font-size: 26px;
+            color: $colorB;
+          }
+        }
+        .order-content {
+          padding: 0 43px;
+          .good-box {
+            width: 88%;
+            .good-list {
+              display: flex;
+              align-items: center;
+              height: 145px;
+              .good-img {
+                width: 112px;
+                img {
+                  width: 100%;
                 }
               }
-            }
-            .good-state{
-              @include height(145px);
-              font-size: 20px;
-              color:$colorA;
-              a{
-                color:$colorA;
+              .good-name {
+                font-size: 20px;
+                color: $colorB;
               }
             }
           }
+          .good-state {
+            @include height(145px);
+            font-size: 20px;
+            color: $colorA;
+            a {
+              color: $colorA;
+            }
+          }
         }
-        .pagination{
-          text-align:right;
-        }
-        .el-pagination.is-background .el-pager li:not(.disabled).active{
-          background-color: #FF6600;
-        }
-        .el-button--primary{
-          background-color: #FF6600;
-          border-color: #FF6600;
-        }
-        .load-more,.scroll-more{
-          text-align:center;
-        }
+      }
+      .pagination {
+        text-align: right;
+      }
+      .el-pagination.is-background .el-pager li:not(.disabled).active {
+        background-color: #ff6600;
+      }
+      .el-button--primary {
+        background-color: #ff6600;
+        border-color: #ff6600;
+      }
+      .load-more,
+      .scroll-more {
+        text-align: center;
       }
     }
   }
+}
 </style>
