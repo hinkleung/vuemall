1e8f6051075f904bcded2bdcec859cf24af967f4
chenql <394869042@qq.com>
2020-07-06 23:28:41 +0800

    15-4 路由按需加载方法

--- src/router.js
+++ src/router.js
@@ -1,21 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './pages/home'
-import Login from './pages/login'
 import Index from './pages/index'
-import Product from './pages/product'
-import Detail from './pages/detail'
-import Cart from './pages/cart'
-import Order from './pages/order'
-import OrderConfirm from './pages/orderConfirm'
-import OrderList from './pages/orderList'
-import OrderPay from './pages/orderPay'
-import AliPay from './pages/alipay'
-
 Vue.use(Router);
 
 export default new Router({
-  routes: [
+  routes:[
     {
       path:'/',
       name:'home',
@@ -23,57 +13,56 @@ export default new Router({
       redirect:'/index',
       children:[
         {
-          path:'/index',
-          name:'index',
-          component:Index,
-        },{
-          path:'/product/:id',
-          name:'product',
-          component:Product,
-        },{
-          path:'/detail/:id',
-          name:'detail',
-          component:Detail,
+          path: '/index',
+          name: 'index',
+          component: Index,
+        }, {
+          path: '/product/:id',
+          name: 'product',
+          component: () => import('./pages/product.vue')
+        }, {
+          path: '/detail/:id',
+          name: 'detail',
+          component: () => import('./pages/detail.vue')
         }
       ]
     },
     {
-      path:'/login',
-      name:'login',
-      component:Login,
+      path: '/login',
+      name: 'login',
+      component: () => import('./pages/login.vue')
     },
     {
-      path:'/cart',
-      name:'cart',
-      component:Cart,
+      path: '/cart',
+      name: 'cart',
+      component: () => import('./pages/cart.vue')
     },
     {
-      path:'/order',
-      name:'order',
-      component:Order,
+      path: '/order',
+      name: 'order',
+      component: () => import('./pages/order.vue'),
       children:[
         {
-          path:'list',
-          name:'order-list',
-          component:OrderList,
+          path: 'list',
+          name: 'order-list',
+          component: () => import('./pages/orderList.vue')
         },
         {
-          path:'confirm',
-          name:'order-confirm',
-          component:OrderConfirm,
+          path: 'confirm',
+          name: 'order-confirm',
+          component: () => import('./pages/orderConfirm.vue')
         },
         {
-          path:'pay',
-          name:'order-pay',
-          component:OrderPay,
+          path: 'pay',
+          name: 'order-pay',
+          component: () => import('./pages/orderPay.vue')
         },
         {
-          path:'alipay',
-          name:'alipay',
-          component:AliPay,
+          path: 'alipay',
+          name: 'alipay',
+          component: () => import('./pages/alipay.vue')
         }
       ]
     }
-    
   ]
-})
+});
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-    devServer:{
-        host:'localhost',
-        port:8080,
-        proxy:{
-            '/api':{
-                target:'http://mall-pre.springboot.cn',
-                changeOrigin:true,
-                pathRewrite:{
-                    '/api':''
-                }
-            }
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://mall-pre.springboot.cn',
+        changeOrigin: true,
+        pathRewrite: {
+          '/api': ''
         }
+      }
     }
+  },
+  productionSourceMap:false
 }
